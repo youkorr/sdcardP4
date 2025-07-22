00b52a9e5c4b345d0fa16e0e06f800e76e8a4ea8
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-22 03:24:03 +0200

    Update sdcardP4.h

--- components/sdcardP4/sdcardP4.h
+++ components/sdcardP4/sdcardP4.h
@@ -7,38 +7,28 @@
 namespace esphome {
 namespace sdcardP4 {
 
-class SdcardP4Component : public Component, 
-    public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, 
-                         spi::CLOCK_POLARITY_LOW, 
-                         spi::CLOCK_PHASE_LEADING, 
-                         spi::DATA_RATE_1MHZ> {
+class SdcardP4Component : public Component, public spi::SPIDevice<> {
  public:
   void setup() override;
-  void loop() override;
   void dump_config() override;
-  float get_setup_priority() const override;
+  float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
   void set_cs_pin(GPIOPin *cs_pin) { this->cs_pin_ = cs_pin; }
-
-  // Tes méthodes existantes...
+  
+  // API publique
   bool initialize_card();
-  bool read_sector(uint32_t sector, uint8_t *buffer);
-  bool write_sector(uint32_t sector, const uint8_t *buffer);
-  uint32_t get_card_size();
-  bool is_card_present();
+  bool is_ready() const { return this->card_ready_; }
 
  protected:
   GPIOPin *cs_pin_{nullptr};
-  bool card_initialized_{false};
-  uint32_t card_size_{0};
-
-  uint8_t send_command(uint8_t cmd, uint32_t arg);
+  bool card_ready_{false};
+  
   void select_card();
   void deselect_card();
-  bool wait_ready();
-  void send_initial_clock();
+  uint8_t send_command(uint8_t cmd, uint32_t arg);
 };
 
 }  // namespace sdcardP4
 }  // namespace esphome
+
 
