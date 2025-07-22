868cce49a948b90b443e572f7601394bf4ac44f7
youkorr <96953496+youkorr@users.noreply.github.com>
2025-07-22 02:56:32 +0200

    Create __init__.py

---
+++ components/sdcardP4/__init__.py
@@ -0,0 +1,23 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import spi
+from esphome.const import CONF_CS_PIN, CONF_ID
+from esphome import pins
+
+DEPENDENCIES = ["spi"]
+
+sdcardP4_ns = cg.esphome_ns.namespace("sdcardP4")
+SdcardP4Component = sdcardP4_ns.class_("SdcardP4Component", cg.Component, spi.SPIDevice)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SdcardP4Component),
+    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
+}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)
+    
+    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    cg.add(var.set_cs_pin(cs_pin))
